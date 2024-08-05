b6b6145b7ea3f2bd72282cacf9f89d1004e773f1
iskongling <145578694+iskongling@users.noreply.github.com>
2024-08-05 16:35:16 +0800

    Update x_3.m

--- x_3.m
+++ x_3.m
@@ -1,7 +1,7 @@
-%Çé¾°3ÏÂGs¶Ô¾ùºâ½âµÄÓ°Ïì,Gs=40,50,60,70£¬Õâ¸öÊÇÉÌ¼ÒoµÄ±ä»¯
+%æƒ…æ™¯3ä¸‹Gså¯¹å‡è¡¡è§£çš„å½±å“,Gs=40,50,60,70ï¼Œè¿™ä¸ªæ˜¯å•†å®¶oçš„å˜åŒ–
 clc;
 clear;
-r0=10,r1=90,r2=70,g0=40,g1=90,g2=80,k1=10,k2=20;
+r0=40,r1=90,r2=70,g0=40,g1=90,g2=80,k1=10,k2=20;
 set(0,'defaultfigurecolor','w')
 % the 1st X,Y
 [t,y]=ode45(@(t,y)twopinzuan(t,y,r0,r1,r2,g0,g1,g2,k1,k2),[0,0.5],[0.5,0.5]);
@@ -14,7 +14,7 @@ hold on
 set(gca,'XTick',[0:0.05:0.5],'YTick',[0:0.1:1.1])
 axis([0 0.5 0 1.1])
 % the 2nd X,Y
-r0=15,r1=90,r2=70,g0=40,g1=90,g2=80,k1=10,k2=20;
+r0=40,r1=90,r2=70,g0=50,g1=90,g2=80,k1=10,k2=20;
 [t,y]=ode45(@(t,y)twopinzuan(t,y,r0,r1,r2,g0,g1,g2,k1,k2),[0,0.5],[0.5,0.5]);
 points=1:1:length(t);
 figure(2)
@@ -23,7 +23,7 @@ hold on
 %plot(t,y(:,2),'bo-.','linewidth',1,'markersize',5,'markerindices',points);
 %hold on
 % the 3rd X,Y
-r0=20,r1=90,r2=70,g0=40,g1=90,g2=80,k1=10,k2=20;
+r0=40,r1=90,r2=70,g0=60,g1=90,g2=80,k1=10,k2=20;
 [t,y]=ode45(@(t,y)twopinzuan(t,y,r0,r1,r2,g0,g1,g2,k1,k2),[0,0.5],[0.5,0.5]);
 points=1:1:length(t);
 figure(2)
@@ -32,7 +32,7 @@ hold on
 %plot(t,y(:,2),'ko-.','linewidth',1,'markersize',5,'markerindices',points);
 %hold on
 % the 4nd X,Y
-r0=25,r1=90,r2=70,g0=40,g1=90,g2=80,k1=10,k2=20;
+r0=40,r1=90,r2=70,g0=70,g1=90,g2=80,k1=10,k2=20;
 [t,y]=ode45(@(t,y)twopinzuan(t,y,r0,r1,r2,g0,g1,g2,k1,k2),[0,0.5],[0.5,0.5]);
 points=1:1:length(t);
 figure(2)
